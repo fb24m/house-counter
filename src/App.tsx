df21e9a7091ca28300da231359efb8f14ff9002d
fb24m <fakem1ner@yandex.ru>
2024-05-15 03:00:48 +0400

    switch theme to dark

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { Button, Card, CssVarsProvider, getInitColorSchemeScript } from '@mui/joy';
 import episodes from './episodes.json'
 
 function remainEvents<T,>(startDate: Date, events: Array<T>) {
@@ -29,28 +30,38 @@ function App() {
 
   console.log(remainEvents(new Date(2024, 3, 12), episodes.episodes))
 
+  console.log(getInitColorSchemeScript())
+
   return (
-    <div className="bg-gray-900 flex items-center justify-center bg-opacity-50 p-4 h-[100vh]">
-      <div className="flex max-w-[720px] flex-col-reverse md:flex-row justify-center items-center p-8 gap-6 bg-gray-900 bg-opacity-50 rounded-3xl">
-        <div className="inline-grid grid-cols-6 gap-1">
-          {remainEvents(new Date(2024, 3, 30), episodes.episodes).map((day: any) =>
-            <div className="group relative inline-block p-2 px-2 rounded-xl text-sm bg-gray-800 text-gray-300">
-              {day.date.getDate() <= 9 ? '0' + day.date.getDate() : day.date.getDate()}.{day.date.getMonth() <= 9 ? '0' + day.date.getMonth() : day.date.getMonth()}
-              <div className="shadow-lg left-0 top-full w-[300px] rounded-xl pointer-events-none z-50 absolute bg-gray-800 -translate-y-2 group-hover:translate-y-2 opacity-0 group-hover:opacity-100 p-2 flex flex-col transition-all duration-300">
-                {day.events.map((event: any) =>
-                  <div key={JSON.stringify(event)} className="p-1 px-2 rounded-lg">
-                    <div className="flex justify-between"><span>{event.name}</span> <span className="text-gray-400">{event.season}.{event.episode_number}</span> </div>
-                    <span className="text-gray-400 text-xs block">({event.date})</span>
-                  </div>
-                )}
+    <div className="bg-[rgb(11,13,14)]">
+      <CssVarsProvider defaultMode="dark" defaultColorScheme="dark" modeStorageKey="dark">
+        <div className="flex items-center justify-center bg-opacity-50 p-4 h-[100vh]">
+          <Card variant="outlined" className="max-w-[720px] p-8" >
+            <div className="flex flex-col-reverse md:flex-row justify-center gap-6 items-center">
+              <div className="inline-grid grid-cols-6 gap-1">
+                {remainEvents(new Date(2024, 3, 30), episodes.episodes).map((day: any) =>
+                  <Button variant="soft" className="group relative inline-block p-2 px-2 rounded-xl text-sm bg-gray-800 text-gray-300">
+                    {day.date.getDate() <= 9 ? '0' + day.date.getDate() : day.date.getDate()}.{day.date.getMonth() <= 9 ? '0' + day.date.getMonth() : day.date.getMonth()}
+                    <div className="absolute left-0 pointer-events-none top-full z-50">
+                      <Card className="shadow-lg w-[300px] bg-gray-800 -translate-y-2 group-hover:translate-y-2 opacity-0 group-hover:opacity-100 flex flex-col transition-all duration-300">
+                        {day.events.map((event: any) =>
+                          <div key={JSON.stringify(event)}>
+                            <div className="flex justify-between"><span>{event.name}</span> <span className="text-gray-400">{event.season}.{event.episode_number}</span> </div>
+                            <span className="text-gray-400 text-left text-xs block w-full">({event.date})</span>
+                          </div>
+                        )}
+                      </Card>
+                    </div>
+                  </Button>)}
               </div>
-            </div>)}
+              <div className="p-2">
+                <div className="font-house-md text-5xl">House1</div>
+                <div className="font-house-md text-xl mt-2 ml-1 [letter-spacing:-0.2em]">PROGRESS</div>
+              </div>
+            </div>
+          </Card>
         </div>
-        <div className="p-2">
-          <div className="font-house-md text-5xl">House1</div>
-          <div className="font-house-md text-xl mt-2 ml-1 [letter-spacing:-0.2em]">PROGRESS</div>
-        </div>
-      </div>
+      </CssVarsProvider>
     </div>
   )
 }
