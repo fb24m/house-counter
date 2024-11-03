04e40cc34a29d5e76f40043b6ace0b3bb40f9ace
fb24m <hello@fb24m.ru>
2024-11-03 13:36:25 +0400

    rebrand to battle creek counter and remove house parts

--- src/App.tsx
+++ src/App.tsx
@@ -114,10 +114,4 @@ function App() {
   )
 }
 
-function App() {
-  return (
-    <iframe className="w-full h-[100vh] object-cover" width="560" height="315" src="https://www.youtube.com/embed/dQw4w9WgXcQ?autoplay=1&mute=1" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe>
-  )
-}
-
 export default App
